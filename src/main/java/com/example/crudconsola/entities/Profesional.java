0d94dfd405772f1d9e96159157af4c5cdf74c075
=Christian Magro Pérez <cmagrop@gmail.com>
2024-06-10 12:42:16 -0400

    Se agrearon entidades Cliente, Especialidad y Profesional. Se avanzo en Especialidad

---
+++ src/main/java/com/example/crudconsola/entities/Profesional.java
@@ -0,0 +1,9 @@
+package com.example.crudconsola.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profesional")
+public class Profesional {
+}
