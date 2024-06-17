20e246377c2c7df084b81fcdf4434e775378f664
=Christian Magro Pérez <cmagrop@gmail.com>
2024-06-17 12:32:18 -0400

    Definicion de metodos findAll y findById en ClienteServiceImpl

--- src/main/java/com/example/crudconsola/services/ClienteServiceImpl.java
+++ src/main/java/com/example/crudconsola/services/ClienteServiceImpl.java
@@ -1,19 +1,28 @@
 package com.example.crudconsola.services;
 
 import com.example.crudconsola.entities.Cliente;
+import com.example.crudconsola.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public class ClienteServiceImpl implements ClienteService{
-    @Override
-    public List<Cliente> findAll() {
-        return List.of();
-    }
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
+    @Transactional(readOnly = true)
+    public List<Cliente> findAll() {
+        return (List<Cliente>)clienteRepository.findAll();
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
     public Optional<Cliente> findById(Integer id) {
-        return Optional.empty();
+        return clienteRepository.findById(id);
     }
 
     @Override
